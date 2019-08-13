b780f757fb485ee3d85e4ca31b73ddd153b67599
kassehpoor <kasehpoor.k@gmail.com>
2019-08-13 18:26:51 +0430

    separate file component on project

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-//import React, { component } from 'react';
-import Home from './Home';
-import About from './About';
-import Topics from './Topics';
 
 function App() {
   return (
@@ -18,6 +12,42 @@ function App() {
         <Route path="/topics" component={Topics} />
       </div>
     </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
   );
 }
 
